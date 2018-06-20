cda95b9862d2bbb7f7aac7ac4de55fcf8067b01e
Nica <veronica.amenamen@yahoo.com>
2018-06-20 11:41:52 +0800

    website 1st sprint

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Complete Bootstrap 4 Website Layout</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Navigation -->
+<nav class="navbar sticky-top navbar-expand-md navbar-dark bg-dark">
+	<div class="container-fluid"> 
+		<a href="#" class="navbar-brand"><img src="img/logo-white.png"></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Services</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          <a class="dropdown-item" href="#">Kliq</a>
+			          <a class="dropdown-item" href="#">Tally</a>
+			          <a class="dropdown-item" href="#">Q</a>
+			          <a class="dropdown-item" href="#">Chat</a>
+			          <a class="dropdown-item" href="#">Pin</a>
+			          <a class="dropdown-item" href="#">Connectivity</a>
+			        </div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Products</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          <a class="dropdown-item" href="#">Reach</a>
+			         
+			        </div>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link">Blogs</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link">Contact Us</a>
+				</li>
+				<li class="nav-item ">
+					<a href="#" class="nav-link loginbord">Log In</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link createaccountbord">Create Account</a>
+				</li>
+				
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<section id="headerimg">
+	<div class="container">
+		<br>
+		<h1> Connect and start doing business 
+with 90 million mobile phone
+users in the country. </h1>
+		 <p>Get the leverage to acquire new customers, engage with exisiting ones and directly impact 
+your target market right atyour fingertips. Mobile360 SMS’ full suite solution helps you create 
+SMS campaigns to improve customer relationships and increase profits of your business. </p>
+
+<br>
+		<a href="" class="btn create-an-account">Create An Account Now </a>
+	</div>
+	
+</section>
+
+
+
+<!--- Why Mobile360 SMS -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<br>
+			<h2 class="my-4" >Why Mobile360 Microservices</h2>
+			<hr class="my-line">
+		</div>
+		<div class="my-space"></div>
+		
+	</div>
+</div>
+
+<!--- Four Column Section -->
+<div class="container-fluid padding">
+	<div class="row text-center padding">
+		<div class="col-xs-12 col-sm-6 col-md-3">
+			
+			<img src="img/acquire-new-customers.png" class="thumbnail">
+			<h5 class="my-4">Acquire New Customers</h5>
+			<hr class="my-line-4">
+			<p>Attract millions of mobile phone users with customized SMS campaigns</p>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-3">
+			<img src="img/enhance-brand-awareness.png" class="thumbnail">
+			<h5 class="my-4">Enhance Brand Awareness</h5>
+			<hr class="my-line-4">
+			<p>Expand the reach of your products and services through mobile connectivity</p>
+		</div>
+		<div class="col-sm-6 col-md-3">
+			
+			<img src="img/flexible-platform.png" class="thumbnail">
+			<h5 class="my-4">Flexible Platform</h5>
+			<hr class="my-line-4">
+			<p>Cultivate customers loyalty by running promos, contests and raffles.</p>
+		</div>
+		<div class="col-sm-6 col-md-3">
+			<img src="img/merge-channels.png" class="thumbnail">
+			<h5 class="my-4">Merge Channels</h5>
+			<hr class="my-line-4">
+			<p>Intensify your campaign via social media and email</p>
+		</div>
+	</div>
+	<div class="my-space"></div>
+</div>
+
+
+
+
+<!--- Three Column Section -->
+<div class="container-fluid padding" style="background:#D9D9D9;">
+	<div class="row text-center padding">
+		<div class="col-12">
+			<div class="my-space"></div>
+			<h2 class="my-4" >What We Offer</h2>
+			<hr class="my-line">
+			<div class="my-space"></div> 
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-3">
+			
+			<img src="img/kliq.png" class="thumbnail">
+			<h5 class="my-4">KLIQ</h5>
+			<hr class="my-line-4">
+			<p>Built with the latest version of HTML, HTML5.</p>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-3">
+			<img src="img/tally.png" class="thumbnail">
+			<h5 class="my-4">TALLY</h5>
+			<hr class="my-line-4">
+			<p>Built with the latest version of Bootstrap, Bootstrap 4.</p>
+		</div>
+		<div class="col-sm-6 col-md-3">
+			
+			<img src="img/q.png" class="thumbnail">
+			<h5 class="my-4">Q</h5>
+			<hr class="my-line-4">
+			<p>Built with the latest version of CSS, CSS 3.</p>
+		</div>
+		<div class="col-sm-6 col-md-3">
+			<img src="img/reach.png" class="thumbnail">
+			<h5 class="my-4">REACH</h5>
+			<hr class="my-line-4">
+			<p>Built with the latest version of CSS, CSS 3.</p>
+		</div>
+
+
+
+		<div class="col-xs-12 col-sm-6 col-md-4">
+			
+			<img src="img/pin.png" class="thumbnail">
+			<h5 class="my-4">PIN</h5>
+			<hr class="my-line-4">
+			<p>Your new best friend for two-way verification.</p>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-4">
+			<img src="img/chat.png" class="thumbnail">
+			<h5 class="my-4">CHAT</h5>
+			<hr class="my-line-4">
+			<p>Let your customers be heard, interacting with millions of mobile subscribers made easy.</p>
+		</div>
+		<div class="col-sm-12 col-md-4">
+			
+			<img src="img/integration.png" class="thumbnail">
+			<h5 class="my-4">INTEGRATION</h5>
+			<hr class="my-line-4">
+			<p>Direct. Reliable. Built for volume.</p>
+		</div>
+
+
+	</div>
+	<div class="my-space"></div>
+</div>
+
+
+</body>
+</html>
